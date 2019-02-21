5e90831ef9f4a66f9d0d82436f38a704765f7168
OuadhiSamy <samyouadhi@gmail.com>
2019-02-22 00:18:51 +0100

    Animation container kine. Ajout plugin splash, page compte

--- www/php/authentification.php
+++ www/php/authentification.php
@@ -1,33 +1,35 @@
 <?php
+  header("Access-Control-Allow-Origin: *");
+  header('Access-Control-Allow-Methods: GET, POST');
+  // $host =  "localhost";
+  // $dbname = "ekine";
+  // $user = "root";
+  // $password = "";
+  // //
+  // // $email = $_POST['email'];
+  // // $mdp = $_POST['mdp'];
 
-  $host =  "localhost";
-  $dbname = "ekine";
-  $user = "root";
-  $password = "";
 
-  $email = $_POST['email'];
-  $mdp = $_POST['mdp'];
+  // try {
+  //   $connect = new PDO("msql:dbname=$dbname;host=$server");
+  // } catch (PDOExeption $e) {
+  //   echo 'Erreur lors de la connexion : '. $e->getMessage();
+  // }
+  //
+  // if (isset($_POST['login'])) {
+  //   $requete = "SELECT * FROM `login` WHERE `email`='$email' AND `mdp`='$mdp'";
+  //   $connect->querry($requete) as $row;
+  //   echo "truc";
+  //   if ($row !0) {
+  //     echo "succes";
+  //   }
+  //   else {
+  //     echo "error";
+  //   }
+  //
+  //   $connect = null;
+  // }
 
-
-  try {
-    $connect = new PDO("msql:dbname=$dbname;host=$server");
-  } catch (PDOExeption $e) {
-    echo 'Erreur lors de la connexion : '. $e->getMessage();
-  }
-
-  if (isset($_POST['login'])) {
-    $requete = "SELECT * FROM `login` WHERE `email`='$email' AND `mdp`='$mdp'";
-    $connect->querry($requete) as $row;
-    echo "truc";
-    if ($row !0) {
-      echo "succes";
-    }
-    else {
-      echo "error";
-    }
-
-    $connect = null;
-  }
-
+  echo "truc";
 
  ?>
