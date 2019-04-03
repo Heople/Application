a50d6a8e7ac6f2fba05ed351f95c4f566f50459e
OuadhiSamy <samyouadhi@gmail.com>
2019-04-03 03:38:29 +0200

    Correction du bug url (ajout locationProvider), css quasi fini, pb video

--- platforms/browser/www/controllers/exercicecontroller.js
+++ platforms/browser/www/controllers/exercicecontroller.js
@@ -35,7 +35,7 @@
 
 
     $scope.exercices=[
-        {nom: "Triple salto du bras gauche", description:"Debout ou assis, le dos bien droit, inclinez la tête sur un côté et tirez doucement avec votre main jusqu’à ressentir l’étirement à la base du cou. Pour plus d’amplitude, la main du côté étiré pourra être placée dans le dos. Étirez de la même façon l’autre côté.", lien: "https://www.youtube.com/watch?v=eLYhnBaWOzc"},
+        {nom: "Triple salto du bras gauche", description:"Faire tournoyer son bras gauche dans le sens antéchronologique des aiguilles d'une montre.", lien: "https://www.youtube.com/watch?v=eLYhnBaWOzc"},
         {nom: "Triple salto du bras droit", description:"Faire tournoyer son bras droit dans le sens antéchronologique des aiguilles d'une montre.", lien: "https://www.youtube.com/watch?v=o85w80g2Aq4"},
         {nom: "Contraction de la fesse droite arrière", description:"Contracter la fesse droite arrière.", lien: "https://www.youtube.com/watch?v=y9eFk8TuV9k"},
         {nom: "Contraction de la fesse gauche arrière", description:"Contracter la fesse gauche arrière.", lien: "https://www.youtube.com/watch?v=PJielywHIjY"},
--- www/controllers/programmecontroller.js
+++ www/controllers/programmecontroller.js
@@ -1,10 +1,29 @@
 var app = angular.module('ProgrammeGenerator', ['ngRoute', 'ngYoutubeEmbed']);
+app.config(['$routeProvider', '$locationProvider', function AppConfig($routeProvider, $locationProvider) {
+    // enable html5Mode for pushstate ('#'-less URLs)
+    $locationProvider.html5Mode(true);
+}]);
+
+
 app.controller('ProgrammeController', function($scope){
 
-  $scope.programme = [
-    {id: 0,nom: 'exercice1', description: 'exercice1', repetition: 10, temps: 30, url: 'https://www.youtube.com/watch?v=XWEkGT7XP08'},
-    {id: 1,nom: 'exercice2', description: 'exercice2', repetition: 5, temps: 10, url: "https://www.youtube.com/watch?v=PJielywHIjY"},
-    {id: 2,nom: 'exercice3', description: 'exercice3', repetition: 20, temps: 60, url: "https://www.youtube.com/watch?v=o85w80g2Aq4"},
+
+
+  $scope.programme=[
+      {id: 0, nom: "Triple salto du bras gauche", description:"Debout ou assis, le dos bien droit, inclinez la tête sur un côté et tirez doucement avec votre main jusqu’à ressentir l’étirement à la base du cou. Pour plus d’amplitude, la main du côté étiré pourra être placée dans le dos. Étirez de la même façon l’autre côté.", lien: "https://www.youtube.com/embed/7UxfCZ4ADXs"},
+      {id: 1, nom: "Triple salto du bras droit", description:"Faire tournoyer son bras droit dans le sens antéchronologique des aiguilles d'une montre.", lien: "https://www.youtube.com/embed/7UxfCZ4ADXs"},
+      {id: 2, nom: "Contraction de la fesse droite arrière", description:"Contracter la fesse droite arrière.", lien: "https://www.youtube.com/embed/7UxfCZ4ADXs"},
+      {id: 3, nom: "Contraction de la fesse gauche arrière", description:"Contracter la fesse gauche arrière.", lien: "https://www.youtube.com/embed/7UxfCZ4ADXs"},
   ]
+
+  // angular.forEach($scope.exercices, function(item){
+  //         $scope.videoURL = item.lien;
+  //         $scope.$digest();
+  //
+  //
+  //
+  // })
+
+
 
 });
